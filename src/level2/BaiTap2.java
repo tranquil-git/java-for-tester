7a225686d7f435ab57a8722df44174d5beb2276b
Trân <48040329+tranquil-git@users.noreply.github.com>
2026-05-05 20:56:27 +0700

    Update BaiTap2.java

--- src/level2/BaiTap2.java
+++ src/level2/BaiTap2.java
@@ -6,10 +6,25 @@ import java.util.Set;
 
 public class BaiTap2 {
     public static void main(String[] args){
-        System.out.print("Input: ");
+        System.out.print("Input a string: ");
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        String s = sc.nextLine();
+        //
+        System.out.println("Is it a Palindrome?");
+        System.out.print(isPalindrome(s));
+    }
 
-        
+    static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right){
+            if (s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
