71b8467ff8b78109e6147aa88956d8146f4ff1f6
Trân <48040329+tranquil-git@users.noreply.github.com>
2026-05-05 20:22:39 +0700

    Create BaiTap2.java

---
+++ src/level2/BaiTap2.java
@@ -0,0 +1,15 @@
+package level2;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class BaiTap2 {
+    public static void main(String[] args){
+        System.out.print("Input: ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        
+    }
+}
